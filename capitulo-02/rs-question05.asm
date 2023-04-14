ab525de3f16d7cd07aeb0df71fa9ff7052c061c3
Marcus Nogueira <112811596+marcusnogueiraa@users.noreply.github.com>
2023-04-14 16:46:21 -0300

    Update rs-question05.asm

--- capitulo-02/rs-question05.asm
+++ capitulo-02/rs-question05.asm
@@ -7,9 +7,7 @@
         .stack  100h
 printf  PROTO   arg1:Ptr Byte, printlist:VARARG
         .data
-msg1fmt byte 0Ah, "%d%s%d%s%d", 0Ah, 0Ah, 0
-msg1    byte "+", 0
-msg2    byte "=", 0
+msg1fmt byte 0Ah, "%d + %d = %d", 0Ah, 0Ah, 0
 x       sdword ?
 y       sdword ?
 z       sdword ?
@@ -18,7 +16,7 @@ main    proc
 mov     x,1
 mov     y,2
 mov     z,3
-        INVOKE printf, ADDR msg1fmt, x, ADDR msg1, y, ADDR msg2, z
+        INVOKE printf, ADDR msg1fmt, x, y, z
         ret
 main    endp
         end
