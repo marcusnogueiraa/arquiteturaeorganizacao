fffbec4c6557687fdfcf20b176ecb490f488e7b1
Marcus Nogueira <112811596+marcusnogueiraa@users.noreply.github.com>
2023-04-14 17:49:02 -0300

    Update rs-question08.asm

--- capitulo-02/rs-question08.asm
+++ capitulo-02/rs-question08.asm
@@ -1,1 +1,29 @@
-; Por enquanto, não há nada aqui.
+;
+;   Building Instructions:
+;   ml /Cx /coff rs-question08.asm /link /SUBSYSTEM:console /out:a.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
+;
+        .686
+        .model  flat, c
+        .stack  100h
+printf  PROTO   arg1:Ptr Byte, printlist:VARARG
+scanf   PROTO arg2:Ptr Byte, inputlist:VARARG
+        .data
+in1fmt byte "%d",0
+msg0fmt byte "%s",0
+msg1fmt byte 0Ah,"Numbers",0
+msg2fmt byte 0Ah,"   %d",0
+msg0    byte "Enter a number: ",0
+num1    sdword ?
+num2    sdword ?
+        .code
+main    proc
+        INVOKE printf, ADDR msg0fmt, ADDR msg0
+        INVOKE scanf, ADDR in1fmt, ADDR num1
+        INVOKE printf, ADDR msg0fmt, ADDR msg0
+        INVOKE scanf, ADDR in1fmt, ADDR num2
+        INVOKE printf, ADDR msg1fmt
+        INVOKE printf, ADDR msg2fmt, num1
+        INVOKE printf, ADDR msg2fmt, num2
+        ret
+main    endp
+        end
