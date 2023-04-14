8ecb4e6668317f1375549d4a4a85b21940e410fd
André Alves <91325610+AndreAlves-18@users.noreply.github.com>
2023-04-14 09:30:56 -0300

    Update rs-question05.asm

--- capitulo-02/rs-question05.asm
+++ capitulo-02/rs-question05.asm
@@ -1,1 +1,24 @@
-; Por enquanto, não há nada aqui.
+;
+;   Building Instructions:
+;   ml /Cx /coff rs-question05.asm /link /SUBSYSTEM:console /out:a.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
+;
+        .686
+        .model  flat, c
+        .stack  100h
+printf  PROTO   arg1:Ptr Byte, printlist:VARARG
+        .data
+msg1fmt byte 0Ah, "%d%s%d%s%d", 0Ah, 0Ah, 0
+msg1    byte "+", 0
+msg2    byte "=", 0
+x       sdword ?
+y       sdword ?
+z       sdword ?
+        .code
+main    proc
+mov     x,1
+mov     y,2
+mov     z,3
+        INVOKE printf, ADDR msg1fmt, x, ADDR msg1, y, ADDR msg2, z
+        ret
+main    endp
+        end
