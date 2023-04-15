e649a483f894f5813cd999c13b5851158dc448e8
Marcus Nogueira <112811596+marcusnogueiraa@users.noreply.github.com>
2023-04-15 15:16:08 -0300

    Update rs-question05.asm

--- capitulo-03/rs-question05.asm
+++ capitulo-03/rs-question05.asm
@@ -1,1 +1,27 @@
-; Por enquanto, não há nada aqui.
+;
+;   Building Instructions:
+;   ml /Cx /coff rs-question05.asm /link /SUBSYSTEM:console /out:a.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
+;
+        .686
+        .model  flat, c
+        .stack  100h
+printf  PROTO arg1:Ptr Byte, printlist:VARARG
+scanf   PROTO arg2:Ptr Byte, inputlist:VARARG
+        .data
+in1fmt   byte "%d",0
+msg0fmt  byte 0Ah,"Enter an integer: ",0
+msg1fmt  byte 0Ah,"The integer is: %d",0Ah,0Ah,0
+number   sdword ?
+        .code
+main    proc
+        INVOKE printf, ADDR msg0fmt
+        INVOKE scanf, ADDR in1fmt, ADDR number
+        mov eax,3
+        imul number
+        mov ebx,7
+        sub ebx,eax
+        mov number,ebx
+        INVOKE printf, ADDR msg1fmt, number
+        ret
+main    endp
+        end
