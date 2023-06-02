6b204f551d63e465f1a8a0e6445ab7e786b0c0c7
Marcus Nogueira <112811596+marcusnogueiraa@users.noreply.github.com>
2023-06-02 10:19:52 -0300

    Create rs-question09.asm

---
+++ capitulo-05/rs-question09.asm
@@ -0,0 +1,33 @@
+;
+;   Building Instructions:
+;   ml /Cx /coff rs-question09.asm /link /SUBSYSTEM:console /out:rs-question09.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
+;
+        .686
+        .model  flat, c
+        .stack  100h
+printf  PROTO   arg1:Ptr Byte, printlist:VARARG
+scanf   PROTO   arg2:Ptr Byte, inputlist:VARARG
+        .data
+msg1fmt byte    "Digite o numero para calcular o fatorial: ",0
+msg2fmt byte    0Ah,"O fatorial desse numero e: %d",0
+in1fmt  byte    "%d",0
+result  sdword  1
+numFat  sdword  ?
+        .code
+main    proc
+        INVOKE printf, ADDR msg1fmt
+        INVOKE scanf, ADDR in1fmt, ADDR numFat
+        .if numFat > 1
+        .while numFat > 1
+        mov eax,result
+        imul numFat
+        mov result,eax
+        dec numFat
+        .endw
+        .else
+        mov eax, numFat
+        mov result, eax
+        .endif
+        INVOKE printf, ADDR msg2fmt, result
+main    endp
+        end
